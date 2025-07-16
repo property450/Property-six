7148c8d540c643287816c9ff31b5baea666a95cc
property450 <leeyongweilee@gmail.com>
2025-07-16 16:20:34 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,10 +6,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// ✅ 创建客户端
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
