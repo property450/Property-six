8a220d96e2173d27f812e2601680ee3d807bcd69
property450 <leeyongweilee@gmail.com>
2025-07-21 11:11:51 +0800

    Update MapWithMarkers.js

--- components/MapWithMarkers.js
+++ components/MapWithMarkers.js
@@ -1,13 +1,11 @@
 // components/MapWithMarkers.js
 import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import 'leaflet/dist/leaflet.css';
 
-// 动态导入 react-leaflet 和 leaflet，只在客户端运行
 const MapWithMarkersClient = dynamic(() => import('./MapWithMarkersClient'), {
-  ssr: false,
+  ssr: false,
 });
 
 export default function MapWithMarkers(props) {
-  return <MapWithMarkersClient {...props} />;
+  return <MapWithMarkersClient {...props} />;
 }
