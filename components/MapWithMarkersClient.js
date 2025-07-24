b1768ae3b23219843d0902b0d49f0a2b4d305c4f
property450 <leeyongweilee@gmail.com>
2025-07-24 21:43:32 +0800

    Update MapWithMarkersClient.js

--- components/MapWithMarkersClient.js
+++ components/MapWithMarkersClient.js
@@ -1,48 +1,59 @@
-// components/MapWithMarkersClient.js
-import { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Circle, Popup, useMap } from 'react-leaflet';
+'use client';
+
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
-function ChangeView({ center, zoom }) {
-  const map = useMap();
-  useEffect(() => {
-    if (center) {
-      map.setView(center, zoom);
-    }
-  }, [center, zoom, map]);
-  return null;
-}
+const defaultCenter = { lat: 3.139, lng: 101.6869 };
+const defaultRadius = 5000;
 
-export default function MapWithMarkersClient({ properties, center, radius }) {
-  const mapRef = useRef();
+export default function MapWithMarkersClient({ properties, center = defaultCenter, radius = defaultRadius }) {
+  const [mapCenter, setMapCenter] = useState(defaultCenter);
+  const [filteredProperties, setFilteredProperties] = useState([]);
 
-  return (
-    <MapContainer
-      center={center || [3.139, 101.6869]} // Default: Kuala Lumpur
-      zoom={13}
-      scrollWheelZoom={true}
-      style={{ height: '500px', width: '100%' }}
-      ref={mapRef}
-    >
-      <ChangeView center={center} zoom={13} />
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {center && radius && (
-        <Circle center={center} radius={radius} pathOptions={{ color: 'blue', fillOpacity: 0.2 }} />
-      )}
-      {properties?.map((property) => (
-        <Marker key={property.id} position={[property.latitude, property.longitude]}>
-          <Popup>
-            <strong>{property.title}</strong><br />
-            {property.address}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+  useEffect(() => {
+    if (center?.lat && center?.lng) {
+      setMapCenter(center);
+    }
+  }, [center]);
+
+  useEffect(() => {
+    const filtered = properties.filter((p) => {
+      if (!p.latitude || !p.longitude) return false;
+      const dist = L.latLng(center.lat, center.lng).distanceTo([p.latitude, p.longitude]);
+      return dist <= radius;
+    });
+    setFilteredProperties(filtered);
+  }, [center, radius, properties]);
+
+  return (
+    <MapContainer
+      center={[mapCenter.lat, mapCenter.lng]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: '600px', width: '100%', borderRadius: '12px' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Circle center={[mapCenter.lat, mapCenter.lng]} radius={radius} pathOptions={{ color: 'blue' }} />
+
+      {filteredProperties.map((property) => (
+        <Marker key={property.id} position={[property.latitude, property.longitude]}>
+          <Popup>
+            <div>
+              <strong>{property.title}</strong>
+              <br />
+              {property.address}
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
