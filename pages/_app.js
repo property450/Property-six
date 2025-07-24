98dc2032726da59d85275ee356847fbeda90e220
property450 <leeyongweilee@gmail.com>
2025-07-24 11:40:10 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { supabase } from '../supabaseClient';
 import Header from '../components/Header';
 import '../styles/globals.css';
+import 'leaflet/dist/leaflet.css'; // ✅ 加上这行以加载地图样式
 
 function MyApp({ Component, pageProps }) {
   return (
