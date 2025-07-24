4a7e5b49e29893c9d4a7d6ca6c0b3255baac993a
property450 <leeyongweilee@gmail.com>
2025-07-24 21:52:19 +0800

    Update MapWithMarkersClient.js

--- components/MapWithMarkersClient.js
+++ components/MapWithMarkersClient.js
@@ -1,55 +1,63 @@
+// components/MapWithMarkersClient.js
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Circle, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet/dist/leaflet.css';
 
-const defaultCenter = { lat: 3.139, lng: 101.6869 };
-const defaultRadius = 5000;
-
-export default function MapWithMarkersClient({ properties, center = defaultCenter, radius = defaultRadius }) {
-  const [mapCenter, setMapCenter] = useState(defaultCenter);
-  const [filteredProperties, setFilteredProperties] = useState([]);
-
+export default function MapWithMarkersClient({ center, properties, radius }) {
   useEffect(() => {
-    if (center?.lat && center?.lng) {
-      setMapCenter(center);
-    }
-  }, [center]);
+    delete L.Icon.Default.prototype._getIconUrl;
 
-  useEffect(() => {
-    const filtered = properties.filter((p) => {
-      if (!p.latitude || !p.longitude) return false;
-      const dist = L.latLng(center.lat, center.lng).distanceTo([p.latitude, p.longitude]);
-      return dist <= radius;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+      iconUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+      shadowUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     });
-    setFilteredProperties(filtered);
-  }, [center, radius, properties]);
+  }, []);
+
+  if (!center) {
+    return (
+      <div className="text-center mt-10 text-gray-500">Enter an address to view map.</div>
+    );
+  }
 
   return (
     <MapContainer
-      center={[mapCenter.lat, mapCenter.lng]}
+      center={[center.lat, center.lng]}
       zoom={13}
       scrollWheelZoom={true}
-      style={{ height: '600px', width: '100%', borderRadius: '12px' }}
+      style={{ height: '600px', width: '100%' }}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Circle center={[mapCenter.lat, mapCenter.lng]} radius={radius} pathOptions={{ color: 'blue' }} />
+      {/* 中心点圆圈 */}
+      <Circle
+        center={[center.lat, center.lng]}
+        radius={radius * 1000}
+        pathOptions={{ fillColor: 'blue', color: 'blue', fillOpacity: 0.2 }}
+      />
 
-      {filteredProperties.map((property) => (
-        <Marker key={property.id} position={[property.latitude, property.longitude]}>
+      {/* 房源标记 */}
+      {properties.map((property) => (
+        <Marker
+          key={property.id}
+          position={[property.latitude, property.longitude]}
+        >
           <Popup>
             <div>
               <strong>{property.title}</strong>
               <br />
               {property.address}
+              <br />
+              RM {property.price}
             </div>
           </Popup>
         </Marker>
