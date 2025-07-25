27e359321194786d6392e0438669463ad70f59fb
property450 <leeyongweilee@gmail.com>
2025-07-25 19:02:10 +0800

    Create geocode.js

---
+++ utils/geocode.js
@@ -0,0 +1,15 @@
+export async function geocodeAddress(address) {
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`
+  );
+  const data = await response.json();
+
+  if (data && data.length > 0) {
+    return {
+      lat: parseFloat(data[0].lat),
+      lng: parseFloat(data[0].lon),
+    };
+  } else {
+    throw new Error('Geocoding failed: Address not found.');
+  }
+}
