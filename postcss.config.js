bae9d09c540bb2eaa07d841891cd74f64304babc
property450 <leeyongweilee@gmail.com>
2025-07-25 22:21:13 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 // postcss.config.js
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
