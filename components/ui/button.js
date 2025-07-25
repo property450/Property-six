8d1db0f59950893e183beec1a7ec68e6a49791ad
property450 <leeyongweilee@gmail.com>
2025-07-25 15:17:28 +0800

    Create button.js

---
+++ components/ui/button.js
@@ -0,0 +1,10 @@
+export function Button({ children, ...props }) {
+  return (
+    <button
+      {...props}
+      className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+    >
+      {children}
+    </button>
+  );
+}
