530ff63508ff255d0550a95e444f24945cc740f3
property450 <leeyongweilee@gmail.com>
2025-07-26 15:47:48 +0800

    Update MapWithMarkersClient.js

--- components/MapWithMarkersClient.js
+++ components/MapWithMarkersClient.js
@@ -1,4 +1,3 @@
-// components/MapWithMarkersClient.js
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
@@ -7,44 +6,51 @@ import L from "leaflet";
 import { useEffect, useState } from "react";
 
 const markerIcon = new L.Icon({
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+  crossOrigin: true,
 });
 
 export default function MapWithMarkersClient({ properties, center, radius }) {
-  const [mapCenter, setMapCenter] = useState(center);
+  if (!center) return <p className="text-center mt-4">Please enter an address to search.</p>;
 
-  useEffect(() => {
-    if (center && center.lat && center.lng) {
-      setMapCenter(center);
-    }
-  }, [center]);
-
-  return (
-    <MapContainer center={mapCenter} zoom={13} style={{ height: "600px", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-      />
-      {radius && center && (
-        <Circle center={center} radius={radius} pathOptions={{ color: 'blue', fillOpacity: 0.1 }} />
-      )}
-      {properties.map((property) => (
-        <Marker
-          key={property.id}
-          position={[property.lat, property.lng]}
-          icon={markerIcon}
-        >
-          <Popup>
-            <div>
-              <h3>{property.title}</h3>
-              <p>{property.price} RM</p>
-              <p>{property.address}</p>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+  return (
+    <MapContainer
+      center={center}
+      zoom={13}
+      style={{ height: "600px", width: "100%" }}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {radius && center && (
+        <Circle
+          center={center}
+          radius={radius * 1000}
+          pathOptions={{ color: "blue", fillOpacity: 0.1 }}
+        />
+      )}
+      {properties.map((property) => (
+        <Marker
+          key={property.id}
+          position={[Number(property.lat), Number(property.lng)]}
+          icon={markerIcon}
+        >
+          <Popup>
+            <div>
+              <h3 className="font-bold">{property.title}</h3>
+              <p>{property.price} RM</p>
+              <p>{property.address}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
