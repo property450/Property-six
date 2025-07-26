16e9f78a2592292ba9b4c102d131260376addfbb
property450 <leeyongweilee@gmail.com>
2025-07-26 13:19:52 +0800

    Update DistanceSelector.js

--- components/DistanceSelector.js
+++ components/DistanceSelector.js
@@ -1,19 +1,23 @@
+// components/DistanceSelector.js
 import React from 'react';
-import { Input } from '@/components/ui/input';
 
-export default function DistanceSelector({ distance, setDistance }) {
+const DistanceSelector = ({ distance, setDistance }) => {
   return (
     <div className="mb-4">
-      <label className="block mb-1 font-medium text-sm">Search Radius (km)</label>
-      <Input
-        type="number"
+      <label className="block mb-1 text-sm font-medium">Search Radius (km)</label>
+      <select
         value={distance}
-        min={1}
-        max={100}
-        step={1}
         onChange={(e) => setDistance(Number(e.target.value))}
-        className="w-full"
-      />
+        className="w-full p-2 border rounded"
+      >
+        {[1, 3, 5, 10, 20, 30, 50].map((d) => (
+          <option key={d} value={d}>
+            {d} km
+          </option>
+        ))}
+      </select>
     </div>
   );
-}
+};
+
+export default DistanceSelector;
