47881b83cb781770d90342b7119e267beb627b00
property450 <leeyongweilee@gmail.com>
2025-07-26 21:29:55 +0800

    Update RoomCountSelector.js

--- components/RoomCountSelector.js
+++ components/RoomCountSelector.js
@@ -5,7 +5,7 @@ export default function RoomCountSelector({ label, value, onChange }) {
     <div className="space-y-2">
       <label>{label}</label>
       <select
-        value={count}
+        value={value}
         onChange={(e) => onChange(e.target.value)}
         className="w-full border p-2 rounded"
       >
