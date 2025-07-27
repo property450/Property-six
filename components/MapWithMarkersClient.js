6c779250647aba52a99d6668dde139d54050060d
property450 <leeyongweilee@gmail.com>
2025-07-27 19:46:29 +0800

    Update MapWithMarkersClient.js

--- components/MapWithMarkersClient.js
+++ components/MapWithMarkersClient.js
@@ -6,19 +6,27 @@ import L from "leaflet";
 import { useEffect, useState } from "react";
 
 export default function MapWithMarkersClient({ properties, center, radius }) {
-const markerIcon = new L.Icon({
+  const markerIcon = new L.Icon({
     iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  shadowSize: [41, 41],
-  crossOrigin: true,
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    shadowSize: [41, 41],
+    crossOrigin: true,
   });
+
   if (!center || !radius) {
-  return <p className="text-center mt-4">Please enter an address to search.</p>;
-}
-  const centerLatLng = L.latLng(center.lat || center[0], center.lng || center[1]);
+    return <p className="text-center mt-4">Please enter an address to search.</p>;
+  }
+
+  const centerLatLng = L.latLng(
+    Number(center.lat ?? center[0]),
+    Number(center.lng ?? center[1])
+  );
+
+  console.log("🧭 Center:", centerLatLng);
+  console.log("📍 Properties:", properties);
 
   return (
     <MapContainer
@@ -32,34 +40,45 @@ const markerIcon = new L.Icon({
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      {radius && center && (
-        <Circle
-          center={centerLatLng}
-          radius={radius * 1000}
-          pathOptions={{ color: "blue", fillOpacity: 0.1 }}
-        />
-      )}
+      <Circle
+        center={centerLatLng}
+        radius={radius * 1000}
+        pathOptions={{ color: "blue", fillOpacity: 0.1 }}
+      />
 
       {properties
+        ?.filter(
+          (property) =>
+            property.lat != null &&
+            property.lng != null &&
+            !isNaN(property.lat) &&
+            !isNaN(property.lng)
+        )
         .filter((property) => {
-          const distance = L.latLng(property.lat, property.lng).distanceTo(centerLatLng);
+          const distance = L.latLng(
+            Number(property.lat),
+            Number(property.lng)
+          ).distanceTo(centerLatLng);
           return distance <= radius * 1000;
         })
-        .map((property) => (
-          <Marker
-            key={property.id}
-            position={[property.lat, property.lng]}
-            icon={markerIcon}
-          >
-            <Popup>
-              <div>
-                <h3 className="font-bold">{property.title}</h3>
-                <p>{property.price} RM</p>
-                <p>{property.address}</p>
-              </div>
-            </Popup>
-          </Marker>
-        ))}
+        .map((property) => {
+          console.log("📌 Showing Marker:", property.title);
+          return (
+            <Marker
+              key={property.id}
+              position={[Number(property.lat), Number(property.lng)]}
+              icon={markerIcon}
+            >
+              <Popup>
+                <div>
+                  <h3 className="font-bold">{property.title}</h3>
+                  <p>{property.price} RM</p>
+                  <p>{property.address}</p>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
     </MapContainer>
   );
 }
