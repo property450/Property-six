0fbd29faaa1fffc8c81fa6858103e8b725310bf8
property450 <leeyongweilee@gmail.com>
2025-07-29 14:19:18 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,28 @@ import '@/styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import Header from '@/components/Header';
+import { UserProvider } from '@supabase/auth-helpers-react'; // ✅ 加上这行
+import { supabase } from '../supabaseClient'; // ✅ 确保也引入了 Supabase 客户端
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // 只在浏览器端执行
-    if (typeof window !== 'undefined') {
-      // 动态导入 Leaflet
-      import('leaflet').then(L => {
-        delete L.Icon.Default.prototype._getIconUrl;
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('leaflet').then(L => {
+        delete L.Icon.Default.prototype._getIconUrl;
 
-        L.Icon.Default.mergeOptions({
-          iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-          iconUrl: require('leaflet/dist/images/marker-icon.png'),
-          shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-        });
-      });
-    }
-  }, []);
+        L.Icon.Default.mergeOptions({
+          iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+          iconUrl: require('leaflet/dist/images/marker-icon.png'),
+          shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+        });
+      });
+    }
+  }, []);
 
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
+  return (
+    <UserProvider supabaseClient={supabase}> {/* ✅ 包裹页面 */}
+      <Header />
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
