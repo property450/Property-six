620419456a71a13cc6fd573ffe88940709a84344
property450 <leeyongweilee@gmail.com>
2025-07-30 09:25:55 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { supabase } from "../supabaseClient";
-import Input from "@/components/ui/input";
-import Button from "@/components/ui/button";
+//import Input from "@/components/ui/input";
+//import Button from "@/components/ui/button";
 //import PriceRangeSelector from "@/components/PriceRangeSelector";
 //import TypeSelector from "@/components/TypeSelector";
 
