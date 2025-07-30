ec1b3fa9d2ca2269e1cf3d790f87d4a3a048ba7e
property450 <leeyongweilee@gmail.com>
2025-07-30 16:45:35 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,18 @@ import '@/styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import Header from '@/components/Header';
-import { supabase } from '../supabaseClient'; // 如果有用到 Supabase，可保留
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       import('leaflet').then(L => {
         delete L.Icon.Default.prototype._getIconUrl;
+
         L.Icon.Default.mergeOptions({
           iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
           iconUrl: require('leaflet/dist/images/marker-icon.png'),
@@ -20,9 +25,9 @@ export default function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <>
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionContextProvider>
   );
 }
