1c25d0add5d945c3c093ee64fadc9324943e86bd
property450 <leeyongweilee@gmail.com>
2025-07-30 16:38:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
+// pages/_app.js
 import '@/styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import Header from '@/components/Header';
-import { supabase } from '../supabaseClient'; // ✅ 确保也引入了 Supabase 客户端
+import { supabase } from '../supabaseClient'; // 如果有用到 Supabase，可保留
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       import('leaflet').then(L => {
         delete L.Icon.Default.prototype._getIconUrl;
-
         L.Icon.Default.mergeOptions({
           iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
           iconUrl: require('leaflet/dist/images/marker-icon.png'),
@@ -20,8 +20,9 @@ export default function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
+    <>
       <Header />
       <Component {...pageProps} />
-    </UserProvider>
+    </>
   );
 }
