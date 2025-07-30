180ac5e9b00d885df1b341507d0982c8cdf09865
property450 <leeyongweilee@gmail.com>
2025-07-30 17:02:22 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,33 @@
 // pages/_app.js
 import '@/styles/globals.css';
 import 'leaflet/dist/leaflet.css';
-import { useEffect } from 'react';
-import Header from '@/components/Header';
+
+import { useEffect, useState } from 'react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
+import Header from '@/components/Header';
 
 export default function MyApp({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('leaflet').then(L => {
-        delete L.Icon.Default.prototype._getIconUrl;
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('leaflet').then(L => {
+        delete L.Icon.Default.prototype._getIconUrl;
 
-        L.Icon.Default.mergeOptions({
-          iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-          iconUrl: require('leaflet/dist/images/marker-icon.png'),
-          shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-        });
-      });
-    }
-  }, []);
+        L.Icon.Default.mergeOptions({
+          iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+          iconUrl: require('leaflet/dist/images/marker-icon.png'),
+          shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+        });
+      });
+    }
+  }, []);
 
-  return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  );
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
