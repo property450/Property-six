a0ca9623a3dacca11349c51de3b28cb99a7e4944
property450 <leeyongweilee@gmail.com>
2025-08-27 12:10:16 +0800

    Update CarparkLevelSelector.js

--- components/CarparkLevelSelector.js
+++ components/CarparkLevelSelector.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function CarparkPositionSelector({
+export default function CarparkLevelSelector ({
   value,
   onChange,
   mode = "single", // "single" | "range"
