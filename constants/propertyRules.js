ba2b518f5546d86138ac3e61b0b187b7ea107540
property450 <leeyongweilee@gmail.com>
2025-12-24 17:11:46 +0800

    Create propertyRules.js

---
+++ constants/propertyRules.js
@@ -0,0 +1,16 @@
+// constants/propertyRules.js
+
+export const NEED_STOREYS_CATEGORY = new Set([
+  "Bungalow / Villa",
+  "Semi-Detached House",
+  "Terrace / Link House",
+  "Business Property",
+  "Industrial Property",
+]);
+
+export const ROOM_RENTAL_ELIGIBLE_CATEGORIES = new Set([
+  "Bungalow / Villa",
+  "Apartment / Condo / Service Residence",
+  "Semi-Detached House",
+  "Terrace / Link House",
+]);
