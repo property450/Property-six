2cb33330f18a1e17d904fbde49e41c0ebfb8174d
property450 <leeyongweilee@gmail.com>
2025-12-24 17:34:02 +0800

    Delete components/uploadForms directory

--- components/uploadForms/AuctionPropertyForm.js
+++
@@ -1,8 +0,0 @@
-// components/uploadForms/AuctionPropertyForm.js
-"use client";
-
-import SubsaleSecondaryMarketForm from "@/components/uploadForms/SubsaleSecondaryMarketForm";
-
-export default function AuctionPropertyForm(props) {
-  return <SubsaleSecondaryMarketForm {...props} />;
-}
--- components/uploadForms/RentToOwnSchemeForm.js
+++
@@ -1,8 +0,0 @@
-// components/uploadForms/RentToOwnSchemeForm.js
-"use client";
-
-import SubsaleSecondaryMarketForm from "@/components/uploadForms/SubsaleSecondaryMarketForm";
-
-export default function RentToOwnSchemeForm(props) {
-  return <SubsaleSecondaryMarketForm {...props} />;
-}
