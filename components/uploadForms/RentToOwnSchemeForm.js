fc76fc022d819e18712bcb0ad70f06f695b533b4
property450 <leeyongweilee@gmail.com>
2025-12-24 16:24:01 +0800

    Create RentToOwnSchemeForm.js

---
+++ components/uploadForms/RentToOwnSchemeForm.js
@@ -0,0 +1,8 @@
+// components/uploadForms/RentToOwnSchemeForm.js
+"use client";
+
+import SubsaleSecondaryMarketForm from "@/components/uploadForms/SubsaleSecondaryMarketForm";
+
+export default function RentToOwnSchemeForm(props) {
+  return <SubsaleSecondaryMarketForm {...props} />;
+}
