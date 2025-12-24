340d3dc6dce98be4634c54e1261d53b3d749e98d
property450 <leeyongweilee@gmail.com>
2025-12-24 16:17:59 +0800

    Delete constants directory

--- constants/homestayOptions.js
+++
@@ -1,8 +0,0 @@
-// constants/homestayOptions.js
-
-export const HOMESTAY_TYPE_OPTIONS = [
-  "Entire Place",
-  "Private Room",
-  "Shared Room",
-  "Hotel-like Unit",
-];
--- constants/hotelResortOptions.js
+++
@@ -1,9 +0,0 @@
-// constants/hotelResortOptions.js
-
-export const HOTEL_RESORT_TYPE_OPTIONS = [
-  "Hotel",
-  "Resort",
-  "Serviced Apartment (Hotel-managed)",
-  "Boutique Hotel",
-  "Capsule Hotel",
-];
