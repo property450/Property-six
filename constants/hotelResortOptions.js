3b90f3afa92a51e62175f7317793456c3f357d83
property450 <leeyongweilee@gmail.com>
2025-12-24 14:19:11 +0800

    Create hotelResortOptions.js

---
+++ constants/hotelResortOptions.js
@@ -0,0 +1,9 @@
+// constants/hotelResortOptions.js
+
+export const HOTEL_RESORT_TYPE_OPTIONS = [
+  "Hotel",
+  "Resort",
+  "Serviced Apartment (Hotel-managed)",
+  "Boutique Hotel",
+  "Capsule Hotel",
+];
