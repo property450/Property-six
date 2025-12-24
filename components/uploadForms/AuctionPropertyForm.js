53ecc78dadd25ff0e1ae074a8e0676729d5068ca
property450 <leeyongweilee@gmail.com>
2025-12-24 16:23:35 +0800

    Create AuctionPropertyForm.js

---
+++ components/uploadForms/AuctionPropertyForm.js
@@ -0,0 +1,8 @@
+// components/uploadForms/AuctionPropertyForm.js
+"use client";
+
+import SubsaleSecondaryMarketForm from "@/components/uploadForms/SubsaleSecondaryMarketForm";
+
+export default function AuctionPropertyForm(props) {
+  return <SubsaleSecondaryMarketForm {...props} />;
+}
