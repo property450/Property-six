6e5ae5a6e681d20ae22acb99d3fe188e5a8c1709
property450 <leeyongweilee@gmail.com>
2025-12-24 17:12:55 +0800

    Create propertyCategories.js

---
+++ constants/propertyCategories.js
@@ -0,0 +1,87 @@
+// constants/propertyCategories.js
+
+export const categoryOptions = {
+  Residential: [
+    "Bungalow / Villa",
+    "Apartment / Condo / Service Residence",
+    "Semi-Detached House",
+    "Terrace / Link House",
+  ],
+  Commercial: ["Business Property"],
+  Industrial: ["Industrial Property"],
+  Land: ["Land"],
+  Others: ["Others"],
+};
+
+export const subtypeOptions = [
+  "Penthouse",
+  "Duplex",
+  "Triplex",
+  "Dual Key",
+];
+
+export const CATEGORY_OPTIONS = {
+  "Bungalow / Villa": [
+    "Bungalow",
+    "Link Bungalow",
+    "Twin Villa",
+    "Zero-Lot Bungalow",
+    "Bungalow land",
+  ],
+  "Apartment / Condo / Service Residence": [
+    "Apartment",
+    "Condominium",
+    "Flat",
+    "Service Residence",
+  ],
+  "Semi-Detached House": ["Cluster House", "Semi-Detached House"],
+  "Terrace / Link House": ["Terraced House", "Townhouse"],
+  "Business Property": [
+    "Hotel / Resort",
+    "Hostel / Dormitory",
+    "Boutique Hotel",
+    "Office",
+    "Office Suite",
+    "Business Suite",
+    "Retail Shop",
+    "Retail Space",
+    "Retail Office",
+    "Shop",
+    "Shop / Office",
+    "Sofo",
+    "Soho",
+    "Sovo",
+    "Commercial Bungalow",
+    "Commercial Semi-Detached House",
+    "Mall / Commercial Complex",
+    "School / University",
+    "Hospital / Medical Centre",
+    "Mosque / Temple / Church",
+    "Government Office",
+    "Community Hall / Public Utilities",
+  ],
+  "Industrial Property": [
+    "Factory",
+    "Cluster Factory",
+    "Semi-D Factory",
+    "Detached Factory",
+    "Terrace Factory",
+    "Warehouse",
+    "Showroom cum Warehouse",
+    "Light Industrial",
+    "Heavy Industrial",
+  ],
+  Land: [
+    "Agricultural Land",
+    "Industrial Land",
+    "Commercial Land",
+    "Residential Land",
+    "Oil Palm Estate",
+    "Rubber Plantation",
+    "Fruit Orchard",
+    "Paddy Field",
+    "Vacant Agricultural Land",
+  ],
+};
+
+export const LAYOUT_CATEGORY_OPTIONS = CATEGORY_OPTIONS;
