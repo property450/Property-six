41cd078d0ae7fef59082f1e2d90c44cd18a48953
property450 <leeyongweilee@gmail.com>
2025-12-25 12:48:08 +0800

    Create unitLayoutOptions.js

---
+++ constants/unitLayoutOptions.js
@@ -0,0 +1,75 @@
+// constants/unitLayoutOptions.js
+
+export const CATEGORY_OPTIONS = {
+  "Bungalow / Villa": [
+    "Bungalow",
+    "Link Bungalow",
+    "Twin Villa",
+    "Zero-Lot Bungalow",
+    "Bungalow land",
+  ],
+  "Apartment / Condo / Service Residence": [
+    "Apartment",
+    "Condominium",
+    "Flat",
+    "Service Residence",
+  ],
+  "Semi-Detached House": ["Cluster House", "Semi-Detached House"],
+  "Terrace / Link House": ["Terraced House", "Townhouse"],
+  "Business Property": [
+    "Hotel / Resort",
+    "Hostel / Dormitory",
+    "Boutique Hotel",
+    "Office",
+    "Office Suite",
+    "Business Suite",
+    "Retail Shop",
+    "Retail Space",
+    "Retail Office",
+    "Shop",
+    "Shop / Office",
+    "Sofo",
+    "Soho",
+    "Sovo",
+    "Commercial Bungalow",
+    "Commercial Semi-Detached House",
+    "Mall / Commercial Complex",
+    "School / University",
+    "Hospital / Medical Centre",
+    "Mosque / Temple / Church",
+    "Government Office",
+    "Community Hall / Public Utilities",
+  ],
+  "Industrial Property": [
+    "Factory",
+    "Cluster Factory",
+    "Semi-D Factory",
+    "Detached Factory",
+    "Terrace Factory",
+    "Warehouse",
+    "Showroom cum Warehouse",
+    "Light Industrial",
+    "Heavy Industrial",
+  ],
+  Land: [
+    "Agricultural Land",
+    "Industrial Land",
+    "Commercial Land",
+    "Residential Land",
+    "Oil Palm Estate",
+    "Rubber Plantation",
+    "Fruit Orchard",
+    "Paddy Field",
+    "Vacant Agricultural Land",
+  ],
+};
+
+export const SUBTYPE_OPTIONS = ["Penthouse", "Duplex", "Triplex", "Dual Key"];
+
+export const NEED_STOREYS_CATEGORY = new Set([
+  "Bungalow / Villa",
+  "Business Property",
+  "Industrial Property",
+  "Semi-Detached House",
+  "Terrace / Link House",
+]);
