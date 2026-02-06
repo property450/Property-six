0e12dbfb4a66993a680286c3c51cd17019948443
property450 <leeyongweilee@gmail.com>
2026-02-06 16:18:47 +0800

    Update completedUnit.vm.js

--- utils/property/forms/completedUnit.vm.js
+++ utils/property/forms/completedUnit.vm.js
@@ -1,12 +1,3 @@
 // utils/property/forms/completedUnit.vm.js
 
-import { buildNewProjectVM } from "./newProject.vm";
-
-/**
- * ✅ Completed Unit VM
- * 目前取值逻辑与你 pages/my-profile.js 完全一致，所以复用 buildNewProjectVM
- * 但保持独立入口，未来你要加 Completed Unit 专属显示/字段，改这里就行
- */
-export function buildCompletedUnitVM(rawProperty) {
-  return buildNewProjectVM(rawProperty);
-}
+export { buildVM } from "./newProject.vm";
